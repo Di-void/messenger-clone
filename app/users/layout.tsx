f510496cd36afb526a5b709b0619cc3ac25b5abd
DEE <donzydonathan@gmail.com>
2023-06-09 13:53:08 +0100

    feat: fetch users and pass to client

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,18 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/Sidebar/Sidebar";
+import UserLIst from "./components/UserLIst";
 
 export default async function UserLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
+
   return (
     // @ts-expect-error Server Component
     <Sidebar>
+      <UserLIst items={users} />
       <div className="h-full">{children}</div>
     </Sidebar>
   );
